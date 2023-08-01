2af88f6123388bc7ce92e08bbb69755bb9cdbe8b
Gennadij Ivanov <ivanov.gennadij.v@gmail.com>
2023-08-01 20:30:37 +0200

    get rid of concurrency

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,13 +3,9 @@ package parser
 import (
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/pkg/errors"
-	"golang.org/x/sync/errgroup"
-	"net/http"
-	"sync"
 	"zpcg/internal/parser/detailed_page"
 	"zpcg/internal/parser/general_page"
 	"zpcg/internal/parser/model"
-	"zpcg/internal/utils"
 )
 
 const (
@@ -18,40 +14,30 @@ const (
 )
 
 func ParseTimetable() (map[int]model.DetailedTimetable, error) {
-	generalTimetableResponse, err := http.Get(GeneralTimetablePageUrl)
+	generalTimetableResponse, err := retryablehttp.Get(GeneralTimetablePageUrl)
 	if err != nil {
-		return nil, errors.Wrap(err, "can not get general timetable page with http.Get")
+		return nil, errors.Wrap(err, "can not get general timetable page with retryablehttp.Get")
 	}
 	generalTimetableMap, err := general_page.ParseGeneralTimetablePage(generalTimetableResponse.Body)
 	if err != nil {
 		return nil, errors.Wrap(err, "general_page.ParseGeneralTimetablePage")
 	}
 
-	detailedTimetableMap := sync.Map{}
-	errGroup := errgroup.Group{}
+	detailedTimetableMap := make(map[int]model.DetailedTimetable, len(generalTimetableMap))
+	// do not rewrite this loop with concurrency because zpcg.me do not have enough resources to handle all those requests
 	for routeId, generalTimetable := range generalTimetableMap {
 		routeId, generalTimetable := routeId, generalTimetable
-		errGroup.Go(func() error {
-			response, err := retryablehttp.Get(BaseUrl + generalTimetable.DetailedTimetableLink)
-			if err != nil {
-				return errors.Wrapf(err, "can not get route info with route id = %d, link = %s using http.Get",
-					routeId, generalTimetable.DetailedTimetableLink)
-			}
-			detailedTimetable, err := detailed_page.ParseDetailedTimetablePage(routeId, response.Body)
-			if err != nil {
-				return errors.Wrapf(err, "routeId = %d, link = %s detailed_page.ParseDetailedTimetablePage",
-					routeId, generalTimetable.DetailedTimetableLink)
-			}
-			detailedTimetableMap.Store(detailedTimetable.RouteId, detailedTimetable)
-			return nil
-		})
+		response, err := retryablehttp.Get(BaseUrl + generalTimetable.DetailedTimetableLink)
+		if err != nil {
+			return nil, errors.Wrapf(err, "can not get route info with route id = %d, link = %s using retryablehttp.Get",
+				routeId, generalTimetable.DetailedTimetableLink)
+		}
+		detailedTimetable, err := detailed_page.ParseDetailedTimetablePage(routeId, response.Body)
+		if err != nil {
+			return nil, errors.Wrapf(err, "routeId = %d, link = %s detailed_page.ParseDetailedTimetablePage",
+				routeId, generalTimetable.DetailedTimetableLink)
+		}
+		detailedTimetableMap[detailedTimetable.RouteId] = detailedTimetable
 	}
-	if err := errGroup.Wait(); err != nil {
-		return nil, errors.Wrap(err, "errGroup.Wait")
-	}
-	result, err := utils.SyncMapToMap[int, model.DetailedTimetable](&detailedTimetableMap, len(generalTimetableMap))
-	if err != nil {
-		return nil, errors.Wrap(err, "utils.SyncMapToMap")
-	}
-	return result, nil
+	return detailedTimetableMap, nil
 }
--- internal/utils/map.go
+++ internal/utils/map.go
@@ -1,42 +1,7 @@
 package utils
 
-import (
-	"fmt"
-	"sync"
-)
-
 func AddMap[KeyType comparable, ValueType any](m1 map[KeyType]ValueType, m2 map[KeyType]ValueType) {
 	for key, value := range m2 {
 		m1[key] = value
 	}
-}
-
-func SyncMapToMap[KeyType comparable, ValueType any](syncMap *sync.Map, size int) (map[KeyType]ValueType, error) {
-	result := make(map[KeyType]ValueType, size)
-	var (
-		keyConversionValid   = true
-		valueConversionValid = true
-		syncMapKeyExample    any
-		syncMapValueExample  any
-	)
-	syncMap.Range(func(key, value any) bool {
-		syncMapKeyExample, syncMapValueExample = key, value
-		_key, ok := key.(KeyType)
-		if !ok {
-			keyConversionValid = false
-			return false
-		}
-		_value, ok := value.(ValueType)
-		if !ok {
-			valueConversionValid = false
-			return false
-		}
-		result[_key] = _value
-		return true
-	})
-	if !keyConversionValid || !valueConversionValid {
-		return nil, fmt.Errorf("failed to convert sync.map(%T, %T) to a map[%T]%T",
-			syncMapKeyExample, syncMapValueExample, *new(KeyType), *new(ValueType))
-	}
-	return result, nil
 }
