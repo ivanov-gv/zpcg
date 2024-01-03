3137b25329e5a3cb7b695e3642c94ea73a988634
Gennadij Ivanov <86804723+ivanov-gv@users.noreply.github.com>
2024-01-04 00:30:03 +0100

    Feat/russian language (#7)
    * add russian language messages to the bot
    
    * fix rendering of the russian start and error messages

---
+++ internal/render/deafult.go
@@ -0,0 +1,10 @@
+package render
+
+import "golang.org/x/text/language"
+
+var DefaultLanguageTag = language.English
+
+const (
+	StartMessageDefault = StartMessageEn
+	ErrorMessageDefault = ErrorMessageEn
+)
---
+++ internal/render/ru.go
@@ -0,0 +1,29 @@
+package render
+
+const ErrorMessageRu = `Попробуйте снова - две станции на латинице, через запятую. Вот так:
+Podgorica, Niksic`
+
+const StartMessageRu = "" +
+	"*Расписание электричек Черногории*\n" +
+	"\n" +
+	"Сделан вместе с @Leti\\_deshevle" +
+	"\n" +
+	"Пожалуйста, введите две станции через запятую на латинице: \n" +
+	"\n" +
+	"*Podgorica, Bijelo Polje*\n" +
+	"\n" +
+	"В ответе получите расписание:\n" +
+	"\n" +
+	"Podgorica \\-\\> Bijelo Polje\n" +
+	"[6100](https://zpcg.me/details?timetable=41)  `06:20 \\-\\> 08:38` \n" +
+	"\\.\\.\\.\n" +
+	"\n" +
+	"Не уверены как правильно пишется название станции? Не волнуйтесь, просто напишите как знаете \\- мы догадаемся что вы имели ввиду\\.\n" +
+	"\n" +
+	"*Padagornicccccaa , Belo Pollllllle*\n" +
+	"\n" +
+	"Podgorica \\-\\> Bijelo Polje\n" +
+	"[6100](https://zpcg.me/details?timetable=41)  `06:20 \\-\\> 08:38` \n" +
+	"\\.\\.\\.\n" +
+	"\n" +
+	"Теперь ваша очередь\\!"
