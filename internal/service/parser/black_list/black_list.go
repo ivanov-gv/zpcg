7daa7f849693599a4335dc993d1787e758b538f1
Gennadij Ivanov <ivanov.gennadij.v@gmail.com>
2024-02-26 21:59:20 +0100

    comment blacklist to test aliases

--- internal/service/parser/black_list/black_list.go
+++ internal/service/parser/black_list/black_list.go
@@ -2,7 +2,6 @@ package black_list
 
 import (
 	"github.com/samber/lo"
-	"golang.org/x/text/language"
 
 	"zpcg/internal/model"
 	"zpcg/internal/service/name"
@@ -26,75 +25,75 @@ var (
 	}()
 
 	BlackListedStations = []model.BlackListedStation{
-		{
-			Name:                               "Budva",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Tivat",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Kotor",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Cetinje",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Perast",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Durmitor",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Petrovac",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Ulcinj",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Sveti Stefan",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Becici",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Herceg Novi",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Savnik",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Zabljak",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name:                               "Durmitor",
-			LanguageTagToCustomErrorMessageMap: nil,
-		},
-		{
-			Name: "Tirana",
-			LanguageTagToCustomErrorMessageMap: map[string]string{
-				language.Russian.String(): "С Албанией нет железнодорожного сообщения из Черногории.",
-				language.English.String(): "There are no routes between Montenegro and Albania.",
-			},
-		},
-		{
-			Name: "Sarajevo",
-			LanguageTagToCustomErrorMessageMap: map[string]string{
-				language.Russian.String(): "С Боснией нет железнодорожного сообщения из Черногории.",
-				language.English.String(): "There are no routes between Montenegro and Bosnia and Herzegovina.",
-			},
-		},
+		//{ TODO: uncomment when ready
+		//	Name:                               "Budva",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Tivat",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Kotor",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Cetinje",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Perast",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Durmitor",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Petrovac",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Ulcinj",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Sveti Stefan",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Becici",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Herceg Novi",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Savnik",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Zabljak",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name:                               "Durmitor",
+		//	LanguageTagToCustomErrorMessageMap: nil,
+		//},
+		//{
+		//	Name: "Tirana",
+		//	LanguageTagToCustomErrorMessageMap: map[string]string{
+		//		language.Russian.String(): "С Албанией нет железнодорожного сообщения из Черногории.",
+		//		language.English.String(): "There are no routes between Montenegro and Albania.",
+		//	},
+		//},
+		//{
+		//	Name: "Sarajevo",
+		//	LanguageTagToCustomErrorMessageMap: map[string]string{
+		//		language.Russian.String(): "С Боснией нет железнодорожного сообщения из Черногории.",
+		//		language.English.String(): "There are no routes between Montenegro and Bosnia and Herzegovina.",
+		//	},
+		//},
 	}
 )
